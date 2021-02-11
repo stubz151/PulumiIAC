2b3b35db75a213d092fb23a8d7cf3aa76389c61a
Michael Stubbs <michaels@nebula.co.za>
2021-02-11 09:33:01 +0200

    Initial commit

---
+++ Constants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PulumiIAC
+{
+    public static class Locations
+    {
+        public const string WestUS= "WestUS";
+        public const string WestEurope = "WestEurope";
+        public const string SouthEastAsia = "SouthEastAsia";
+    }
+
+    public static class AppServiceKind
+    {
+        public const string App = "App";
+    }
+
+    public static class AppServiceTier
+    {
+        public const string Basic = "Basic";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using Pulumi.Azure.AppService;
+using Pulumi.Azure.AppService.Inputs;
+using Pulumi.Azure.Automation;
+using Pulumi.Azure.Core;
+using Pulumi.Azure.Storage;
+using System;
+using System.Threading.Tasks;
+using Account = Pulumi.Azure.Automation.Account;
+using AccountArgs = Pulumi.Azure.Automation.AccountArgs;
+
+namespace PulumiIAC
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("starting deployment");
+            string rgName = "PulumiTest";
+            var resourceGroup = CreateResourceGroup(rgName, Locations.WestEurope);
+
+            var plan = GetAppServicePlan("CoolApps", new PlanArgs 
+            { 
+                Location = Locations.WestEurope, Kind = AppServiceKind.App,
+                Sku = new PlanSkuArgs
+                {
+                    Tier = AppServiceTier.Basic,
+                    Size = "B1"
+                }
+            });
+            var storageAccount = GetStorageAccount(rgName);
+            var container = GetContainer(storageAccount.Name, "Messages");
+        }
+
+        static ResourceGroup CreateResourceGroup(string rgName, string locationName) => new ResourceGroup(rgName, new ResourceGroupArgs
+        {
+            Location = locationName,
+        });
+
+        static Plan GetAppServicePlan(string planName, PlanArgs args) => new Plan(planName, args);
+
+        static Container GetContainer(string storageAccountName, string containerName) => new Container(containerName, new ContainerArgs
+        {
+            StorageAccountName = storageAccountName,
+            ContainerAccessType = "private",
+        });
+        static Account GetStorageAccount(string resourceGroupName) => new Account("sa", new AccountArgs
+        {
+            ResourceGroupName = resourceGroupName
+        });
+    }
+}
