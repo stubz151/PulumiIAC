9bac6dd134d4d57681b51bfee7133e004d4e20a0
Michael Stubbs <michaels@nebula.co.za>
2021-02-11 09:46:21 +0200

    pushing update

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
-﻿using Pulumi.Azure.AppService;
+﻿using Pulumi;
+using Pulumi.Azure.AppInsights;
+using Pulumi.Azure.AppService;
 using Pulumi.Azure.AppService.Inputs;
 using Pulumi.Azure.Automation;
 using Pulumi.Azure.Core;
+using Pulumi.Azure.Sql;
 using Pulumi.Azure.Storage;
 using System;
 using System.Threading.Tasks;
@@ -30,8 +33,75 @@ namespace PulumiIAC
                     Size = "B1"
                 }
             });
+
             var storageAccount = GetStorageAccount(rgName, storageAccountName);
+
             var container = GetContainer(storageAccountName, "Messages");
+
+            var appInsights = new Insights("appInsights", new InsightsArgs
+            {
+                ApplicationType = "web",
+                ResourceGroupName = rgName
+            });
+
+            var config = new Config();
+            var username = config.Get("sqlAdmin") ?? "pulumi";
+            var password = config.RequireSecret("sqlPassword");
+
+            var blob = new Blob("messages", new BlobArgs
+            {
+                StorageAccountName = storageAccount.Name,
+                StorageContainerName = container.Name,
+                Type = "Block",
+                Source = new FileArchive("wwwroot"),
+            });
+
+            var codeBlobUrl = SharedAccessSignature.SignedBlobReadUrl(blob, storageAccount);
+
+            var sqlServer = new SqlServer("sql", new SqlServerArgs
+            {
+                ResourceGroupName = rgName,
+                AdministratorLogin = username,
+                AdministratorLoginPassword = password,
+                Version = "12.0",
+            });
+
+            var database = new Database("db", new DatabaseArgs
+            {
+                ResourceGroupName = rgName,
+                ServerName = sqlServer.Name,
+                RequestedServiceObjectiveName = "S0",
+            });
+
+            var app = new AppService("app", new AppServiceArgs
+            {
+                ResourceGroupName = resourceGroup.Name,
+                AppServicePlanId = plan.Id,
+                AppSettings =
+            {
+                {"WEBSITE_RUN_FROM_PACKAGE", codeBlobUrl},
+                {"APPINSIGHTS_INSTRUMENTATIONKEY", appInsights.InstrumentationKey},
+                {"APPLICATIONINSIGHTS_CONNECTION_STRING", appInsights.InstrumentationKey.Apply(key => $"InstrumentationKey={key}")},
+                {"ApplicationInsightsAgent_EXTENSION_VERSION", "~2"},
+            },
+                ConnectionStrings =
+            {
+                new AppServiceConnectionStringArgs
+                {
+                    Name = "db",
+                    Type = "SQLAzure",
+                    Value = Output.Tuple<string, string, string>(sqlServer.Name, database.Name, password).Apply(t =>
+                    {
+                        (string server, string database, string pwd) = t;
+                        return
+                            $"Server= tcp:{server}.database.windows.net;initial catalog={database};userID={username};password={pwd};Min Pool Size=0;Max Pool Size=30;Persist Security Info=true;";
+                    }),
+                },
+            },
+            });
+
+            this.Endpoint = app.DefaultSiteHostname;
+
         }
 
         static ResourceGroup CreateResourceGroup(string rgName, string locationName) => new ResourceGroup(rgName, new ResourceGroupArgs
